1787946bbbca9b0105039b4a1fc88c6576ee7da1
Wang <l.wang1@tue.nl>
2020-12-01 11:57:44 +0100

    Initial

---
+++ GameOfLife.cpp
@@ -0,0 +1,8 @@
+#include "GameOfLife_Cell.h"
+#include "GameOfLife_GameOfLife.h"
+using namespace std;
+
+int main(){
+    cout<<"test"<<endl;
+    return 0;
+}
---
+++ GameOfLife_Cell.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+class Cell{
+    private:
+    public:
+        void testPrint();
+};
+
+void Cell::testPrint(){
+    cout<<"gaem"<<endl;
+    return;
+}
